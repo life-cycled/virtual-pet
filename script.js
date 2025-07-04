86fcce38b01238d2ec9962a99c7a2fb1ac9021fd
ashley <ashlawrence002@gmail.com>
2025-07-04 18:34:29 -0400

    made buttons and progress bars functional

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
+function changeSpriteState(state) {
+    const sprite = document.getElementById("sprite")
+    sprite.className = state;
+
+    sprite.addEventListener(
+    "animationend",
+    function resetToIdle() {
+      sprite.classList.remove(state);
+      sprite.removeEventListener("animationend", resetToIdle);
+    },
+    { once: true }
+  );
+}
+
 function feed() {
-    
+    var bar = document.getElementById("hungerBar");
+    var width = bar.offsetWidth;
+    if (width < 286) {
+        bar.style.width = (width + 10) + "px";
+    }
+}
+
+function play() {
+    var bar = document.getElementById("happinessBar");
+    var width = bar.offsetWidth;
+    if (width < 286) {
+        bar.style.width = (width + 10) + "px";
+    }
+}
+
+function sleep() {
+    var bar = document.getElementById("energyBar");
+    var width = bar.offsetWidth;
+    if (width < 286) {
+        bar.style.width = (width + 10) + "px";
+    }
 }
